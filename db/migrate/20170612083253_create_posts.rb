61db699ed35560a21e1718973ca2f34636730ad1
Natsuki <natsukingly01@gmail.com>
2017-06-12 08:33:44 +0000

    first commit

---
+++ db/migrate/20170612083253_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :posts do |t|
+      t.string :url
+      t.text :question
+      t.text :about
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
